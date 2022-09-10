198672515e44e941e0f5fa6ab6ae5084886d690b
Vantruongg <toiiuvn013@gmail.com>
2022-09-10 16:40:24 +0700

    Palindrome Number

---
+++ LeetcodeDaily/Easy/PalindromeNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetcodeDaily.Easy
+{
+    public static class PalindromeNumberLeetCode
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+
+            int temp = x;
+            int toCompare = 0;
+            do
+            {
+                toCompare = (toCompare * 10) + (temp % 10);
+            }
+            while ((temp /= 10) > 0);
+
+            return x == toCompare;
+        }
+    }
+}
--- LeetcodeDaily/Program.cs
+++ LeetcodeDaily/Program.cs
@@ -7,9 +7,12 @@ namespace LeetcodeDaily
     {
         static void Main(string[] args)
         {
+            //two sum
             var nums = new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
-
             TwoSumLeetCode.TwoSum(nums, 11);
+
+            //Palindrome number
+            PalindromeNumberLeetCode.IsPalindrome(0);
         }
     }
 }
